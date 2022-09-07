fa02602d4554ed47e67709f5723f25107b67cb50
Mykolas <101357393+MStasiulionis@users.noreply.github.com>
2022-09-07 23:22:26 +0300

    update

---
+++ main.js
@@ -0,0 +1,29 @@
+document.querySelector("form").addEventListener("submit", addComment);
+
+
+function emailVisibility(){
+    let checkBox = document.getElementById("input-checked");
+    let x = document.getElementById("input-email");
+  if (checkBox.checked == true){
+    x.style.display = "inline";
+  } else {
+     x.style.display = "none";
+  }
+}
+function addComment(event) {
+    event.preventDefault();
+    let commentList = document.getElementById('comments-list');
+    let newListEl = document.createElement('li');
+    let inputText = event.target.elements.text.value;
+	newListEl.innerHTML = inputText;
+	commentList.appendChild(newListEl);
+	inputText.value = '';
+	let inputChecked = event.target.elements.checkbox.checked;
+	if (inputChecked == true) {
+	    let email = event.target.elements.email.value;
+	    alert('Ačiū už klausimą atsakymą gausite el. paštu: '+email)
+	}
+
+}
+
+
